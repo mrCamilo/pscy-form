d962151fbe169e14a778ff196fc10d58e62900d4
mrCamilo <mcamilo@protonmail.com>
2020-06-19 13:05:41 -0400

    Updates

---
+++ precache-manifest.f4031d1ff72fb4414903cb3ab928b45b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f0a5a930dbf8c100bd0147c912cb68",
+    "url": "/pscy-form/index.html"
+  },
+  {
+    "revision": "f219c37412b1f6437f51",
+    "url": "/pscy-form/static/js/2.ba805a95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pscy-form/static/js/2.ba805a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa51f69ea2a5b7cfe4e",
+    "url": "/pscy-form/static/js/main.cc030ad3.chunk.js"
+  },
+  {
+    "revision": "277541436c3ebe25c49d",
+    "url": "/pscy-form/static/js/runtime-main.6fbf3e13.js"
+  }
+]);
