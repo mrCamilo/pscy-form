81168a17d3565028b437d4f0483923cd833aad1b
mrCamilo <mcamilo@protonmail.com>
2020-06-19 16:11:01 -0400

    Updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
 	<header>
-            <h1>Pediatric Symptom Checklist - Youth Report</h1>
+            <h1>Pediatric Symptom Checklist</h1>
 	    <h3>The questionnaire that follows can be used to see if you are having emotional, attentional, or behavioral difficulties. For each item please mark how often you:</h3>
 	</header>
     )	
