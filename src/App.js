26c172e260d9dedf0006cb22e077a849bda74ed5
mrCamilo <mcamilo@protonmail.com>
2020-06-19 11:32:10 -0400

    a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Header from './components/Header.js'
 import Questionnaire from './components/Questionnaire.js'
-import Results from './components/Results.js'
 
 function App() {
   return (
