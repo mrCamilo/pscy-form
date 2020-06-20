ae6ef51ffa48f9169a2c747ea0158407ae821e6d
mrCamilo <mcamilo@protonmail.com>
2020-06-19 21:28:30 -0400

    style change

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -1,16 +1,11 @@
 import React from "react"
 
 function Conditional(props) {
-	if(props.allFilled === true) {
-	    return (
-	        <h3>You filled out all the radio buttons.</h3>
-	    )
-	}
-	return (
-	    <div>
-		<p>Please answer each question.</p>
-	    </div>
-	)
+    return (
+        <div>
+	    {props.allFilled ? <h3>You filled out all the radio buttons.</h3> : <p>Please answer each question.</p>}
+	</div>
+    )
 }
 
 export default Conditional
